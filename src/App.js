f05ac95d543fdff298a529ca6bc95d70b406b89b
Lucas Domingos <lucas.c.domingos@hotmail.com>
2023-02-16 18:15:59 -0300

    Primeiro Commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* colocando o hello world */}
+      <h1>Hello world</h1>
+      {/* Abaixo apenas comentando o que vem como padraõ no react */}
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edite <code>src/App.js</code> e salve para ser recarregado.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header> */}
+    </div>
+  
+  );
+}
+
+export default App;
